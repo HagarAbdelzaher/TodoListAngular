05316362d13772651cf710888eaaed3f24b84f2f
hagar <hagarr.abdelzaher@gmail.com>
2023-03-23 14:49:37 +0200

    made updates in styling

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,6 +3,7 @@ import { Guid } from 'guid-typescript';
 import { User } from 'src/models/user.model';
 import { NgForm, NgModelGroup } from '@angular/forms';
 import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -11,32 +12,31 @@ import { AuthService } from '../auth.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  
-  user:User|any;
-  constructor(private _auth:AuthService){
-  
+
+  user: User | any;
+  constructor(private _auth: AuthService , private router:Router) {
+
   }
-    
-  
-    //submit() {
-   //   this.user = this.signUpForm.value;
-    //  this.user.id=Guid.create();
-   //   localStorage.setItem(this.user.email.toString(),JSON.stringify(this.user));
-    //  this.signUpForm.reset();
-  //  }
-    submitMyForm(form:NgForm) {
-    
-      this.user = form.value;
-      let data:any=localStorage.getItem(this.user.email.toString());
-      let object:any=JSON.parse(data)
+  submitMyForm(form: NgForm) {
+
+    this.user = form.value;
+    let data: any = localStorage.getItem(this.user.email.toString());
+    let object: any = JSON.parse(data)
 
 
-if(object!=null &&this.user.email===object.email && this.user.password===object.password){
-  localStorage.setItem("currentuser",JSON.stringify(object));
-  console.log('logged in')
-}
-else{
-console.log('Wrong credntials')
-}
-      form.reset()}
+    if (object != null && this.user.email === object.email && this.user.password === object.password) {
+      localStorage.setItem("currentuser", JSON.stringify(object));
+      console.log('logged in')
     }
+    else {
+      console.log('Wrong credntials')
+    }
+    form.reset()
+    this.router.navigate(['/todo']);
+  }
+
+  navigate(){
+    this.router.navigate(['/signup']);
+  }
+  
+}
