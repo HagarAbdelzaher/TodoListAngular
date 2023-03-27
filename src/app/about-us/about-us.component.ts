960642b2f2f2c493504f591c4688640e30b7ea75
hagar <hagarr.abdelzaher@gmail.com>
2023-03-27 23:41:22 +0200

    commit

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-about-us',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./about-us.component.css']
 })
 export class AboutUsComponent {
+
 
 }
